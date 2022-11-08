998e119a07e8909d1a00efb501b93db09766deed
Eryk Kooshin Suguiura <80656019+ErykSuguiura@users.noreply.github.com>
2022-11-08 16:54:25 -0300

    feat: integracao de leds e buzzer

--- Integracao_Esp32/src/main.cpp
+++ Integracao_Esp32/src/main.cpp
@@ -4,12 +4,35 @@
 #define BeltRead_Pin 22
 #define LOADCELL_DOUT_PIN  33
 #define LOADCELL_SCK_PIN  32
+#define LED_BELT_PIN 25
+#define LED_WEIGHT_PIN 26
+#define BUZZER_PIN 27
+
+#define NOTE_C5  523
+int melody[] = {
+  NOTE_C5, 0
+};
+
+int noteDurations[] = {
+  4, 4
+};
+int period = 100;
+unsigned long time_now = 0;
+
+#define WEIGHT_THRESHOLD 5
 
 HX711 scale;
+
+float weightRead = 0;
+bool beltRead = 0;
 
 float calibration_factor = -5450; //mais ou menos o valor para ter saida em kg
 
 void setup() {
+  pinMode(LED_BELT_PIN, OUTPUT);
+  pinMode(LED_WEIGHT_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
   Serial.begin(9600);
 
   pinMode(BeltRead_Pin, INPUT);
@@ -31,42 +54,66 @@ void loop() {
 
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
   Serial.print("Reading: ");
+  weightRead = scale.get_units()*0.453592;
+  time_now = millis();
   
   // Verifica RF1 - Balanca
-  if ((scale.get_units()*0.453592, 3) > 0) {
-    Serial.print("Atende RF1");
+  if (weightRead > 0) {
+    //Serial.print("Atende RF1");
   }
   else{
-    Serial.print("Não atende RF1");  
+    //Serial.print("Não atende RF1");  
   }
 
   // Verifica RNF3 - Balanca 
-  if ((scale.get_units()*0.453592, 3) >= 40)
+  if (weightRead >= WEIGHT_THRESHOLD)
   {
-    Serial.print("Atende RNF3");
+    //Serial.print("Atende RNF3");
+    digitalWrite(LED_BELT_PIN, LOW);
   }
   else{
-    Serial.print("Não atende RNF3");  
+    //Serial.print("Não atende RNF3");  
+    digitalWrite(LED_BELT_PIN, HIGH);
   }
 
-  Serial.print(scale.get_units()*0.453592, 3);
+  Serial.print(weightRead, 3);
   Serial.print(" kg, Belt read: ");
-  Serial.print(digitalRead(BeltRead_Pin));
+
+  beltRead = digitalRead(BeltRead_Pin);
+  Serial.print(beltRead);
 
   // Verifica RF2 - Cinto
-  if (digitalRead(BeltRead_Pin) == 0 || digitalRead(BeltRead_Pin) == 1) {
-    Serial.print("Atende RF2");
+  if (beltRead == 0 || beltRead == 1) {
+    //Serial.print("Atende RF2");
   }
   else{
-    Serial.print("Não atende RF2");  
+    //Serial.print("Não atende RF2");  
   }
 
   // Alerta RF8 - Balanca e Cinto 
-  if (digitalRead(BeltRead_Pin) == 1 && (scale.get_units()*0.453592, 3) > 0) {
-    Serial.print("Atende RF8");
+  if (beltRead == 0 && weightRead > 0) {
+    //Serial.print("Atende RF8");
   }
   else{
-    Serial.print("Não atende RF8");  
+    //Serial.print("Não atende RF8");  
+
+    for (int thisNote = 0; thisNote < 2; thisNote++) 
+    { // MUDAR ISSO AQUI POR CAUSA DO DELAY
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(BUZZER_PIN, melody[thisNote], noteDuration);
+
+      int pauseBetweenNotes = noteDuration * 1.30;
+      
+      while(millis() < time_now + pauseBetweenNotes+10){}
+      noTone(BUZZER_PIN);
+    }
+  }
+
+  if (beltRead == 0) {
+    digitalWrite(LED_WEIGHT_PIN, LOW);
+  }
+  else {
+    digitalWrite(LED_WEIGHT_PIN, HIGH);
   }
 
   Serial.println();
